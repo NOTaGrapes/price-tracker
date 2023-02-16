575dab65a23686a672b71ae066c60d34aa93b634
Nikita <givemeblood57@gmail.com>
2023-02-15 18:18:04 -0600

    quick fix

--- src/UI/Select/Select.jsx
+++ src/UI/Select/Select.jsx
@@ -1,28 +1,34 @@
-import React, {useState,useEffect} from 'react'
-import './Select.css'
+import "./Select.css"
 
-const Select = (props) => {
-	const [options,setOptions] = useState([]);
+import React from "react"
 
-	useEffect(()=>{
-		setOptions(props.options.map((text, index) => {return <option key={index} value={text}>{text}</option>}));
-		console.log("Select component get new options",props.options)
-	},[props.options]);
-
-	return(
+const Select = ({ options, name, defaultValue, onChange }) => {
+  console.log("options", options)
+  return (
     <div>
-		<select 
-		aria-label={props.name}
-		className="Select"
-		options = {props.options}
-		onChange={(e)=>{props.onChange(e.target.value)}}
-		defaultValue={props.defaultValue}
-		>
-		<option key="defaultValue"  value={props.defaultValue} disabled>{props.defaultValue}</option>
-			{options}
-		</select>
-	</div>
-    );
+      <select
+        aria-label={name}
+        className="Select"
+        options={options}
+        onChange={(e) => {
+          onChange(e.target.value)
+        }}
+        defaultValue={defaultValue}
+      >
+        <option key="defaultValue" value={defaultValue} disabled>
+          {defaultValue}
+        </option>
+        {["select symbol 1", "select symbol 2"].map((text, index) => {
+          return (
+            <option key={index} value={text}>
+              {text}
+            </option>
+          )
+        })}
+        {options}
+      </select>
+    </div>
+  )
 }
 
-export default Select;
+export default Select
